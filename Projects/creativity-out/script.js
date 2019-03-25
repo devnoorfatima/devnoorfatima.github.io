b1a36560593be4fec0cab5b5ec1baa291684ffda
Noor Fatima <devnoorfatima@gmail.com>
2019-03-25 19:47:27 +0500

    Added the styles to the project.

--- Projects/creativity-out/script.js
+++ Projects/creativity-out/script.js
@@ -1,26 +1,28 @@
 // header rise
 const header = document.querySelector(".rise");
-header.innerHTML=spranWrap(header.textContent);
-function spranWrap (word) {
-return [...word].map(letter => `<span>${letter}</span>`).join("");
+header.innerHTML = spranWrap(header.textContent);
+
+function spranWrap(word) {
+  return [...word].map(letter => `<span>${letter}</span>`).join("");
 }
 
 // getting the input
-const input =document.querySelector('.input');
-const items =document.querySelector('.items');
-const button =document.getElementsByClassName(".btn");
-function get() {
-    const li = document.createElement('li');
-    var textNode = document.createTextNode(input.value);
-    li.appendChild(textNode);
-    if(input.value == ''){
-        alert('Enter some value please!')
-    }else{
-        items.appendChild(li);
-    }
+const input = document.querySelector('.input');
+const items = document.querySelector('.items');
+const button = document.getElementsByClassName(".btn");
 
-    input.value = '';
-      // Click on a close button to hide the current list item
+function get() {
+  const li = document.createElement('li');
+  var textNode = document.createTextNode(input.value);
+  li.appendChild(textNode);
+  if (input.value == '') {
+    alert('Enter some value please!')
+  } else {
+    items.appendChild(li);
+  }
+
+  input.value = '';
+  // Click on a close button to hide the current list item
   const close = document.getElementsByClassName("close");
   const span = document.createElement("SPAN");
   const txt = document.createTextNode("Remove");
@@ -29,22 +31,22 @@ function get() {
   li.appendChild(span);
 
   for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
+    close[i].onclick = function () {
       var div = this.parentElement;
       div.style.display = "none";
     }
   }
 }
 
-    input.addEventListener("keyup", function(event) {
-    event.preventDefault();
-    if (event.keyCode === 13) {
-        document.getElementsByClassName("btn").click();
-    }
+input.addEventListener("keyup", function (event) {
+  event.preventDefault();
+  if (event.keyCode === 13) {
+    document.getElementsById("btn").click();
+  }
 });
 // Add a "checked" symbol when clicking on a list item
 var list = document.querySelector('ol');
-list.addEventListener('click', function(ev) {
+list.addEventListener('click', function (ev) {
   if (ev.target.tagName === 'LI') {
     ev.target.classList.toggle('checked');
   }
