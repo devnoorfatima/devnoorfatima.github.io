053d64c25d2fb8b2c49f5b8c9659908b2709c680
Noor Fatima <devnoorfatima@gmail.com>
2019-03-28 22:50:11 +0500

    Used the bootstrap classes and columns in calculators project.

--- Projects/calculators/Fuel/fuel.js
+++ Projects/calculators/Fuel/fuel.js
@@ -3,8 +3,11 @@ let fuel = document.getElementById('fuel');
 let efficiency = document.getElementById('efficiency');
 let needed = document.getElementById('fuel-needed');
 let cost = document.getElementById('cost');
-
+let need;
+let price;
 function calculatePrice() {
-    needed.value = Math.floor(distance.value / efficiency.value);
-    cost.value = Math.floor(needed.value * fuel.value);
+    need = Math.floor(distance.value / efficiency.value);
+    needed.value = need + ' Gallons/mile';
+    price = Math.floor(need * fuel.value);
+    cost.value = price + ' $';
 }
