ba4cd9631b834567fcdea38f3b6fdb5ea02ba685
Noor Fatima <devnoorfatima@gmail.com>
2019-04-08 14:24:02 +0500

    Added some more checks in form validations.

--- Projects/shopping-cart/checkout.js
+++ Projects/shopping-cart/checkout.js
@@ -1,6 +1,9 @@
 const phone = document.querySelector('.validation');
 const name = document.querySelector('.name');
 const err = document.querySelector('.err');  
+const lname = document.querySelector('.lname');
+const lerr = document.querySelector('.lerr');  
+
 // Get the input field
 let input = document.querySelector(".caps");
 
@@ -22,21 +25,29 @@ input.addEventListener("keyup", function (event) {
 name.addEventListener("keyup", function () {
     let rgx =/[a-zA-Z]+/g;
     if (rgx.test(name.value)) {
-        console.log(name.value);
-        return true;
+        err.style.display='none';
     } else {
       err.style.display='block';     
     }
 });
 
 
-function validate() {
-        let regex = /^\+(?:[0-9] ?){6,14}[0-9]$/;
-        if (regex.test(phone.value)) {
-            console.log(phone.value);
-            return true;
+lname.addEventListener("keyup", function () {
+    let lrgx =/[a-zA-Z]+/g;
+    if (lrgx.test(lname.value)) {
+        lerr.style.display='none';
+    } else {
+      lerr.style.display='block';     
+    }
+});
+
+
+phone.addEventListener("keyup", function () {
+    let regex = /^\+(?:[0-9] ?){6,14}[0-9]$/;
+    if (regex.test(phone.value)) {
+        txt.style.display='none';  
         } else {
         const txt = document.querySelector('.text'); 
         txt.style.display='block';      
         }     
-    }
+});
