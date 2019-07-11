8f74ffb76560c28334b58a4f01322cdf9a4233a1
Noor Fatima <devnoorfatima@gmail.com>
2019-07-11 22:32:26 +0500

    Optimised the images.

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const imgSrc = ['./shots/1.png ', './shots/2.png', './shots/3.png', './shots/4.png', './shots/5.png',
-    './shots/6.png', './shots/7.png', './shots/8.png', './shots/9.png', './shots/10.png', './shots/11.png', './shots/12.png'
+const imgSrc = ['./shots/1-min.png ', './shots/2-min.png', './shots/3-min.png', './shots/4-min.png', './shots/5-min.png',
+    './shots/6-min.png', './shots/7-min.png', './shots/8-min.png', './shots/9-min.png', './shots/10-min.png', './shots/11-min.png', './shots/12-min.png'
 ]
 const name = ['Shopping Cart', 'Calculators', 'Weather App', 'Javascript Quiz', 'Ajax Type Ahead',
-    'Todo List', 'Random Quote Machine', 'Tomato Timer', 'HTML5 Canvas', 'Javascript Image Gallery', 'Indecision App', 'CSS Clock'
+    'Todo List', 'Quote Machine', 'Tomato Timer', 'HTML5 Canvas', 'Image Gallery', 'Indecision App', 'CSS Clock'
 ]
 const link = ['https://devnoorfatima.github.io/Projects/shopping-cart/', 'https://devnoorfatima.github.io/Projects/calculators/', 'https://devnoorfatima.github.io/Projects/weather-app/', 'https://devnoorfatima.github.io/Projects/Javascript-Quiz/', 'https://devnoorfatima.github.io/Projects/Ajax-type-ahead/',
     'https://devnoorfatima.github.io/Projects/todo-list/', 'https://devnoorfatima.github.io/Projects/quote-machine/', 'https://devnoorfatima.github.io/Projects/tomato-timer/', 'https://devnoorfatima.github.io/Projects/HTML-5-Canvas/', 'https://devnoorfatima.github.io/Projects/Javascript-image-gallery/', 'https://github.com/devnoorfatima/React/tree/master/indecision-app', 'https://devnoorfatima.github.io/Projects/CSS-Clock/'
